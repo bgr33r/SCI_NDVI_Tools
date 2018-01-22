6f78ac338949a6ee17ef21195fb577d503791c22
Burke <burke.greer@gmail.com>
2018-01-22 15:53:11 -0800

    Created Repo

---
+++ NDVI_calc.py
@@ -0,0 +1,63 @@
+###NDVI_calc.py
+#By: Burke Greer
+#this file reads in B5, B4, and QA landsat images and calculates NDVI, anad produces projected geotiff
+
+#inputs for NDVI_calc:
+#sat_data_dir = the directory the landsat data is within
+#NIR_name = a list that contains the b5 file or files names
+#RED_name = a list that contains the b4 file or files names
+#BQA_name = a list that contains the QA file or files names
+#Desired_NA_Value_for_BQA = desired NA value from QA file
+#new_NDVI_name = output file name (currently code is written to output GeoTIFF. Other formats possible with edits
+
+import os
+import numpy as np
+from osgeo import gdal
+import matplotlib.pyplot as plt #for plotting test below
+
+def NDVI_calc(sat_data_dir, NIR_name, RED_name, BQA_name, Desired_NA_Value_for_BQA, new_NDVI_name):
+    name_list = [(x,y,z) for x in NIR_name for y in RED_name for z in BQA_name] #TODO add correct pairing check
+    os.chdir(sat_data_dir) #TODO add arugements for output directory, for use input directory
+    for i in name_list:
+        NIR = gdal.Open(i[0])
+        NIR_array = np.array(NIR.ReadAsArray()).astype(np.float)
+        RED = gdal.Open(i[1])
+        RED_array = np.array(RED.ReadAsArray()).astype(np.float)
+        BQA = gdal.Open(i[2])
+        BQA_array = np.array(BQA.ReadAsArray()).astype(np.float)
+        np.seterr(divide='ignore', invalid='ignore') #set to 'ignore' because NAs in NDVI will throw error
+        NDVI_out = (NIR_array - RED_array) / (NIR_array + RED_array)
+        NDVI_out[BQA_array == 1] = Desired_NA_Value_for_BQA #change where BQA is 1 to desired NAN value (useful for plotting/scales)
+        print(name_list, "Mean: ", np.nanmean(NDVI_out))
+        #project NumPy array and export:
+        driver = gdal.GetDriverByName('GTiff')
+        new_dataset = driver.Create(new_NDVI_name,
+                                    NIR.RasterXSize,  # number of columns
+                                    NIR.RasterYSize,  # number of rows
+                                    1,  # number of bands
+                                    gdal.GDT_Float64)  # datatype of the raster
+        new_dataset.SetProjection(NIR.GetProjection())
+        new_dataset.SetGeoTransform(NIR.GetGeoTransform())
+
+        # Now we need to set the band's nodata value to -1
+        new_band = new_dataset.GetRasterBand(1)
+        new_band.SetNoDataValue(-1)
+        new_band.WriteArray(NDVI_out)
+        return new_band
+        new_dataset = None
+        new_band = None
+
+#Read in landsat files first
+filesindir = os.listdir('C://Users//burke//PycharmProjects//SCI//test_data//Landsat//Santa_Cruz_Island//')
+NIR_name = [s for s in filesindir if "B5" in s]  # NIR names
+RED_name = [s for s in filesindir if "B4" in s]  # RED names
+BQA_name = [s for s in filesindir if "BQA" in s]  #QA name
+
+NDVI = NDVI_calc('C://Users//burke//PycharmProjects//SCI//test_data//Landsat//Santa_Cruz_Island//', NIR_name, RED_name, BQA_name, 'NAN', 'NDVI.tif')
+
+#check output:
+test = gdal.Open('NDVI.tif')
+testarray = np.array(test.ReadAsArray()).astype(np.float)
+
+plt.imshow(testarray)
+plt.colorbar()
---
+++ plot_intercept.py
@@ -0,0 +1,50 @@
+#plot_intercept.py
+#By: Burke Greer
+#Below code intersects site data with NDVI data
+
+#inputs for reprojSites:
+#file_name = is the file name of an excel file containin plot IDs and lat and long (in degrees)
+#sheetname = is the name of the sheet with the data
+#keyname = primary key of the data (plot #)
+#sitecrs = is the crs of the plot data (currently st to WGS84)
+#NDVIcrs = is the crs of hte projected data (area of interest is UTM11)
+
+#inputs for get_value_at_ndvi:
+#ndvi_raster = raster file in same projection as point file (previous step reprojected so they match)
+#pos = is the point file which is the site data
+
+import pandas as pd
+import os
+from shapely.geometry import Point
+import geopandas as gpd
+from osgeo import gdal
+import numpy as np
+
+def reprojSites(file_name, sheetname, keyname, sitecrs, NDVIcrs):
+    df = pd.DataFrame(pd.read_excel(file_name, sheetname = sheetname))
+    df.set_index(keys=keyname)
+    geometry = [Point(xy) for xy in zip(df.long, df.lat)]
+    sites = gpd.GeoDataFrame(df, crs=sitecrs, geometry=geometry)
+    sitesreproj = sites.to_crs(crs=NDVIcrs)
+    return sitesreproj
+
+def get_value_at_ndvi(ndvi_raster, pos):
+    gt = ndvi_raster.GetGeoTransform() #get geotransform from input raster
+    data = np.array(ndvi_raster.ReadAsArray()).astype(np.float) #convert to numpy array
+    x,y = list(map(int, (pos.geometry.x - gt[0])/gt[1])),list(map(int, (pos.geometry.y - gt[3])/gt[5])) #extract xy list of features in pos
+    out = data[y,x] #create output
+    #print(out)
+    return(out)
+
+data_dir = os.path.expanduser('~\\PycharmProjects\\SCI\\test_data\\')
+os.chdir(data_dir)
+
+plots = reprojSites('SCI.xlsx', 'site', 'plot', {'init': 'epsg:4326'}, {'init': 'epsg:26911'})
+
+#load in NDVI.tif
+NDVI = gdal.Open('C://Users//burke//PycharmProjects//SCI//test_data//Landsat//Santa_Cruz_Island//NDVI.tif')
+#extract NDVI for each point in raster:
+plots['NDVI'] = get_value_at_ndvi(NDVI,plots)
+#save to .shp
+#write pandas geodataframe to shapefile
+plots[['plot','geometry','NDVI']].to_file('siteswNDVI.shp', driver='ESRI Shapefile')
