ea2f8ca05255ddace1abd61d58482c6ea89733c1
Burke <burke.greer@gmail.com>
2018-01-22 18:49:23 -0800

    Update NDVI_calc.py

--- NDVI_calc.py
+++ NDVI_calc.py
@@ -19,6 +19,7 @@ def NDVI_calc(sat_data_dir, NIR_name, RED_name, BQA_name, Desired_NA_Value_for_B
     name_list = [(x,y,z) for x in NIR_name for y in RED_name for z in BQA_name] #TODO add correct pairing check
     os.chdir(sat_data_dir) #TODO add arg for output directory, input directory
     for i in name_list:
+        #open NIR, RED, QA data with GDAL, convert to NumPy array
         NIR = gdal.Open(i[0])
         NIR_array = np.array(NIR.ReadAsArray()).astype(np.float)
         RED = gdal.Open(i[1])
@@ -26,10 +27,12 @@ def NDVI_calc(sat_data_dir, NIR_name, RED_name, BQA_name, Desired_NA_Value_for_B
         BQA = gdal.Open(i[2])
         BQA_array = np.array(BQA.ReadAsArray()).astype(np.float)
         np.seterr(divide='ignore', invalid='ignore') #set to 'ignore' because NAs in NDVI will throw error
+        #Calculate NDVI
         NDVI_out = (NIR_array - RED_array) / (NIR_array + RED_array)
         NDVI_out[BQA_array == 1] = Desired_NA_Value_for_BQA #change where BQA is 1 to desired NAN value (useful for plotting/scales/stats)
+        #report names and mean for processing outputs
         print(name_list, "Mean: ", np.nanmean(NDVI_out))
-        #project NumPy array and export:
+        #project NumPy array in GDAL and export:
         driver = gdal.GetDriverByName('GTiff')
         new_dataset = driver.Create(new_NDVI_name,
                                     NIR.RasterXSize,  # number of columns
